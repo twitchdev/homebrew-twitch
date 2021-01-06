33330495456376171b2b4df2988185f8c7d5d67c
lleadbet <lucas.leadbetter@gmail.com>
2021-01-06 11:13:35 -0800

    bumping version of CLI
    Closes #3.
    
    Signed-off-by: Lucas Leadbetter <lucas.leadbetter@gmail.com>

--- Formula/twitch-cli.rb
+++ Formula/twitch-cli.rb
@@ -1,8 +1,8 @@
 class TwitchCli < Formula
   desc "CLI for Twitch's developer offerings"
   homepage "https://github.com/twitchdev/twitch-cli"
-  url "https://github.com/twitchdev/twitch-cli/archive/0.1.0.tar.gz"
-  sha256 "a0aeaadf147f66d072bc6692b7c15141d4b4e2df065b3db6e14e3b2048864b53"
+  url "https://github.com/twitchdev/twitch-cli/archive/0.2.0.tar.gz"
+  sha256 "264acaaa258fd7b82da471ad240d90421bb762860430ddf77846e9cff43b2e02"
   license "Apache-2.0"
 
   bottle do
