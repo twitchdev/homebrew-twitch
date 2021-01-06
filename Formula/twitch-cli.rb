0cb02470c978d2a8c29702ef69fa9276d0276a0e
lleadbet <lucas.leadbetter@gmail.com>
2021-01-06 11:48:40 -0800

    patching to 0.2.1

--- Formula/twitch-cli.rb
+++ Formula/twitch-cli.rb
@@ -1,15 +1,15 @@
 class TwitchCli < Formula
   desc "CLI for Twitch's developer offerings"
   homepage "https://github.com/twitchdev/twitch-cli"
-  url "https://github.com/twitchdev/twitch-cli/archive/0.2.0.tar.gz"
-  sha256 "264acaaa258fd7b82da471ad240d90421bb762860430ddf77846e9cff43b2e02"
+  url "https://github.com/twitchdev/twitch-cli/archive/0.2.1.tar.gz"
+  sha256 "31fd247260d78735ab9e8d7e52c994eed3d17907102f556c7945c1498bef86f7"
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/twitchdev/homebrew-twitch/releases/download/twitch-cli-0.2.0"
+    root_url "https://github.com/twitchdev/homebrew-twitch/releases/download/twitch-cli-0.1.0"
     cellar :any_skip_relocation
-    sha256 "12473d344269a77cdb6f1b608d25167366675332b67de9cc4f6788d87919acc0" => :catalina
-    sha256 "048c1d66a51aed8e959c6cc2804dbb9a50f52ab700b7e56e4cdaf0cb4fe62c86" => :x86_64_linux
+    sha256 "63c018e76da21a6736c18b23877b1edf35edfef6c8d90b7fc2fcef5e894efd73" => :catalina
+    sha256 "7797ffe0e14bdd1e249136e2e670ae0a91c08417463ae88102622302d0a54b26" => :x86_64_linux
   end
 
   depends_on "go" => :build
