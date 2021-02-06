f0d2b220786b0d29502a5747d56d6dce3ff22580
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-05 22:51:19 -0800

    Brew formula update for twitch-cli version v0.3.2

--- Formula/twitch-cli.rb
+++ Formula/twitch-cli.rb
@@ -5,17 +5,17 @@
 class TwitchCli < Formula
   desc "CLI for Twitch's developer offerings"
   homepage "https://github.com/twitchdev/twitch-cli"
-  version "0.3.1"
+  version "0.3.2"
   license "Apache-2.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/twitchdev/twitch-cli/releases/download/v0.3.1/twitch-cli_0.3.1_Darwin_x86_64.tar.gz"
-    sha256 "862fb1ed837638ae047d3f9d59c0523c4dcfa6736ec7652de37b5ef633a403cc"
+    url "https://github.com/twitchdev/twitch-cli/releases/download/v0.3.2/twitch-cli_0.3.2_Darwin_x86_64.tar.gz"
+    sha256 "ddedaae4254bca0c7648e7b88ed9a99d19630d104c3ac687daa3683600a6b81b"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/twitchdev/twitch-cli/releases/download/v0.3.1/twitch-cli_0.3.1_Linux_x86_64.tar.gz"
-    sha256 "0f4b18f5098261f4a9665961d5d5120fa29f4a97f7550dc39e92a270b5cd8cb3"
+    url "https://github.com/twitchdev/twitch-cli/releases/download/v0.3.2/twitch-cli_0.3.2_Linux_x86_64.tar.gz"
+    sha256 "02ed4aaa839574ceda273329d29f3cd84503277a8cd706e50c24e3290e0f3414"
   end
 
   def install
