0785998f00c4d4c761122bbbd5f775a0e3ebeb31
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-07 14:31:20 -0400

    Brew formula update for twitch-cli version 1.1.1

--- Formula/twitch-cli.rb
+++ Formula/twitch-cli.rb
@@ -5,21 +5,21 @@
 class TwitchCli < Formula
   desc "CLI for Twitch's developer offerings"
   homepage "https://github.com/twitchdev/twitch-cli"
-  version "1.1.0"
+  version "1.1.1"
   license "Apache-2.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/twitchdev/twitch-cli/releases/download/1.1.0/twitch-cli_1.1.0_Darwin_x86_64.tar.gz"
-    sha256 "c25e3ae78c7d2437b1585c5e8a407fca336ba173326ed9245c919115661f3ed3"
+    url "https://github.com/twitchdev/twitch-cli/releases/download/1.1.1/twitch-cli_1.1.1_Darwin_x86_64.tar.gz"
+    sha256 "7124c1fda4b0a07a146cea05593d437e5291865408c237c9d8f05a908e36c319"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/twitchdev/twitch-cli/releases/download/1.1.0/twitch-cli_1.1.0_Linux_x86_64.tar.gz"
-    sha256 "291957f9cdc17d32a7fd654aa37035d14f7b2212880594a06574b26e9b06a291"
+    url "https://github.com/twitchdev/twitch-cli/releases/download/1.1.1/twitch-cli_1.1.1_Linux_x86_64.tar.gz"
+    sha256 "6443e5a3cd440ddd2821bdbe91847b0274d4e24b699a3a2d191b08d0e16ca271"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/twitchdev/twitch-cli.git", tag: "1.1.0"
+    url "https://github.com/twitchdev/twitch-cli.git", tag: "1.1.1"
     depends_on "go" => :build
   end
 
